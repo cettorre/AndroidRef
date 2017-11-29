df5a6fdeb74413e4ce8d35fe4015fb62574af856
Unknown <c.ettorre@gmail.com>
2017-11-29 13:25:02 +0100

    working on battery status activity (Main7Activity) and layout
    -Layout:
    added progress bar with battery status
    added TextView with % of battery
    added image view for status full charging default
    
    -Main7Activity:
    created Broadcast Receiver retrieving battery status and level
    registered broadcast receiver on method onStart
    unregistered broadcast receiver on method onStop

--- AndroidRef/app/src/main/java/com/example/busko/androidref/Main7Activity.java
+++ AndroidRef/app/src/main/java/com/example/busko/androidref/Main7Activity.java
@@ -1,13 +1,66 @@
 package com.example.busko.androidref;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Main7Activity extends AppCompatActivity {
+
+    private ProgressBar bar = null;
+    private ImageView status = null;
+    private TextView level = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
+        bar = (ProgressBar) findViewById(R.id.bar);
+        status = (ImageView)findViewById(R.id.status);
+        level = (TextView)findViewById(R.id.level);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(onBatteryChanged, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(onBatteryChanged);
+    }
+
+    BroadcastReceiver onBatteryChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int pct = 100*intent.getIntExtra(BatteryManager.EXTRA_LEVEL,1)/
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE,1);
+
+            bar.setProgress(pct);
+            level.setText(String.valueOf(pct));
+
+            switch (intent.getIntExtra(BatteryManager.EXTRA_STATUS,-1)){
+                case BatteryManager.BATTERY_STATUS_CHARGING:
+                    status.setImageResource(R.drawable.battery_charging);
+                    break;
+                case BatteryManager.BATTERY_STATUS_FULL:
+                    status.setImageResource(R.drawable.battery_full);
+                    break;
+                default:
+                    status.setImageResource(R.drawable.battery_on);
+            }
+        }
+    };
+
+
 }
--- AndroidRef/app/src/main/java/com/example/busko/androidref/MainActivity.java
+++ AndroidRef/app/src/main/java/com/example/busko/androidref/MainActivity.java
@@ -39,10 +39,8 @@ public class MainActivity extends AppCompatActivity {
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent i = new Intent(MainActivity.this , Main2Activity.class);
                 startActivity(i);
-
             }
         });
 
