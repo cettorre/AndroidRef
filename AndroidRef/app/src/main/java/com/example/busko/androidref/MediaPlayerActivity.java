e7db88151b5980daf7f5e13ca645e098dad18b8d
Unknown <c.ettorre@gmail.com>
2018-04-14 11:56:46 +0200

    Media Player
    changed activity_media_player removing Image button to use MediaCOntroller class instead
    
    created mediaController
    
    created method getRawResIdByName to return video file resouce identifier and return as int
    
    setting videoView passing Uri usinf obtained int
    
    setting videoview to start from point 0 and show the mediaController during 15 seconds into OnPreparedListener anonimous class
    added appcompat theme to activity

--- AndroidRef/app/src/main/java/com/example/busko/androidref/MediaPlayerActivity.java
+++ AndroidRef/app/src/main/java/com/example/busko/androidref/MediaPlayerActivity.java
@@ -1,39 +1,118 @@
 package com.example.busko.androidref;
 
-import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class MediaPlayerActivity extends AppCompatActivity implements View.OnClickListener{
+public class MediaPlayerActivity extends AppCompatActivity {
 
-    ProgressDialog mDialog;
-    VideoView videoView;
-    ImageButton btnPlayPause;
-    String videoURL="https://vimeo.com/251646742?ref=fb-share&1";
+
+    private VideoView videoView;
+    private int position = 0;
+    private MediaController mediaController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
 
-        videoView= findViewById(R.id.videoView);
-        btnPlayPause= findViewById(R.id.btn_play_pause);
-        btnPlayPause.setOnClickListener(this);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        // Set the media controller buttons
+        if (mediaController == null) {
+            mediaController = new MediaController(MediaPlayerActivity.this);
+
+            // Set the videoView that acts as the anchor for the MediaController.
+            mediaController.setAnchorView(videoView);
 
 
+            // Set MediaController for VideoView
+            videoView.setMediaController(mediaController);
+        }
+
+
+        try {
+            // ID of video file.
+            int id = this.getRawResIdByName("mountain");
+            videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + id));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoView.requestFocus();
+
+
+        // When the video file ready for playback.
+        videoView.setOnPreparedListener(new OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+
+
+                videoView.seekTo(position);
+                if (position == 0) {
+                    videoView.start();
+                    mediaController.show(15000);
+                }
+
+                // When video Screen change size.
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+                        // Re-Set the videoView that acts as the anchor for the MediaController
+//                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
 
     }
 
+    // Find ID corresponding to the name of the resource (in the directory raw).
+    public int getRawResIdByName(String resName) {
+        String pkgName = this.getPackageName();
+        // Return 0 if not found.
+        int resID = this.getResources().getIdentifier(resName, "raw", pkgName);
+        Log.i("AndroidVideoView", "Res Name: " + resName + "==> Res ID = " + resID);
+        return resID;
+    }
+
+
+    // When you change direction of phone, this method will be called.
+    // It store the state of video (Current position)
     @Override
-    public void onClick(View v) {
-        mDialog= new ProgressDialog(MediaPlayerActivity.this);
-        mDialog.setMessage("loading...");
-        mDialog.setCanceledOnTouchOutside(false);
-        mDialog.show();
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
 
+        // Store current position.
+        savedInstanceState.putInt("CurrentPosition", videoView.getCurrentPosition());
+        videoView.pause();
     }
+
+
+    // After rotating the phone. This method is called.
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Get saved position.
+        position = savedInstanceState.getInt("CurrentPosition");
+        videoView.seekTo(position);
+    }
+
 }
